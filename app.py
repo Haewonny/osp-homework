1f974255cb64369c04ce979741cd990333c8f5d6
Lee Haewon <hawon2205@ewhain.net>
2022-11-08 22:43:58 +0900

    project setting

--- app.py
+++ app.py
@@ -1,35 +1,30 @@
 from flask import Flask, render_template, request
 import sys
 
-application = Flask(__name__)
+app = Flask(__name__)
 
 
-@application.route("/")
+@app.route("/")
 def hello():
     return render_template("index.html")
 
 
-@application.route("/list")
-def view_list():
-    return render_template("list.html")
-
-
-@application.route("/review")
-def view_review():
-    return render_template("review.html")
-
-
-@application.route("/register_restaurant")
+@app.route("/register_restaurant")
 def reg_restaurant():
     return render_template("register_restaurant.html")
 
 
-@application.route("/register_review")
+@app.route("/register_review")
 def reg_review():
     return render_template("register_review.html")
 
 
-@application.route("/submit_restaurant")
+@app.route("/result")
+def reg_result():
+    return render_template("result.html")
+
+
+@app.route("/submit_restaurant")
 def reg_restaurant_submit():
     name = request.args.get("name")
     addr = request.args.get("addr")
@@ -44,4 +39,4 @@ def reg_restaurant_submit():
 
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
